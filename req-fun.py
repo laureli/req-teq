65cbdafa980604c49ef991a60c589424736f30ad
laureli <lk.mallek@gmail.com>
2015-06-13 16:36:19 -0700

    now with the ability to pass in multiple urls at commandline

--- req-fun.py
+++ req-fun.py
@@ -1,16 +1,23 @@
 import requests, sys
 
-#enter url on the commandline
+# enter url on the commandline
 s = str(sys.argv[1])
 
-r = requests.get(s, verify=False)
+for url in range(1, len(sys.argv)):
+    s = sys.argv[url]
+    print s
 
-for h in r.headers:
-  print h,'::',r.headers[h]
-  print '- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -'
+    r = requests.get(s, verify=False)
+    print "The URL you're checking is %s" % s
+    print "Header count is %s" % (len(r.headers))
 
-for i in range(4):
-  print '++++++++++++++++++++'
+    for h in r.headers:
+        print h, '::', r.headers[h]
+        print '- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -'
 
-print r.content
+for i in range(2):
+    print '++++++++++++++++++++'
 
+
+# print the content of the page
+# print r.content
